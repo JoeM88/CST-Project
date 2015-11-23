35e3170b64d9c2b95bf33c8346804ec24c73f91d
Joseph Molina <josephmolina3388@gmail.com>
2015-11-22 18:06:24 -0800

    testing file not required from in this project
    Deleting a file I used to test out image manipulation.

--- Applying filters.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-from PIL import ImageFilter
-
-#Opening up an image and applying a filter to it, then saving it
-img = Image.open("C:\\Users\\Joseph Molina\\Desktop\\CST\\space.jpg")
-imgout = img.filter(ImageFilter.FIND_EDGES)
-img.show()
-imgout.show()
-imgout.save("anything3.jpg")
